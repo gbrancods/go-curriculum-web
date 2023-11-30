f85eddca41d53cc1cc70ed236fe8742f0b9b74d0
Gab Home <gbrancods@gmail.com>
2023-11-30 13:14:36 -0300

    changed port to render

--- router/router.go
+++ router/router.go
@@ -11,6 +11,6 @@ func HandleRequest() (err error) {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/br/", handlers.HomePtBr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
-	err = http.ListenAndServe(":8001", nil)
+	err = http.ListenAndServe(":80", nil)
 	return
 }
